3a6480033cb8daf6397a6d021e28815ef2fb84d5
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-04 06:54:16 +0600

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000; 
+
+const recipe = require('./data/data.json')
+
+app.use(cors()); 
 
 app.get('/', (req, res) => {
   res.send('Mexicano is running');
 }); 
+
+app.get('/recipes', (req, res) => {
+  res.send(recipe)
+})
 
 app.listen(port, () => {
   console.log(`Mexicano API running on port ${port}`);
