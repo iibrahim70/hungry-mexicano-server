72ed3bbd09e4d172116a9f0ff194da2587a8eab1
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-04 23:26:02 +0600

    added vercel config file

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ app.get('/', (req, res) => {
   res.send('Mexicano is running');
 }); 
 
-app.get('/recipes', (req, res) => {
+app.get('/recipe', (req, res) => {
   res.send(recipe)
+})
+
+app.get('/recipe/:id', (req, res) => {
+  const id = parseInt(req.params.id); 
+  console.log(id);
+  const selectedRecipe = recipe.find(n => n.id === id);
+  res.send(selectedRecipe);
 })
 
 app.listen(port, () => {
